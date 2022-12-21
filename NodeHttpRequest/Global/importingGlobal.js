d1f88b729890736a60024bf38a8ccf8c3680a984
Mattheus Soares <mattheussbarbosa@hotmail.com>
2022-12-20 22:31:20 -0300

    att

--- NodeHttpRequest/Global/importingGlobal.js
+++ NodeHttpRequest/Global/importingGlobal.js
@@ -1,5 +1,6 @@
 require("./global") //como fez a importação do global, qualquer coisa que foi criada no modo global ficará pública nesse módulo
 
 console.log(global.MyApp.saudation())
+console.log(`global.MyApp.name = ${global.MyApp.name}`)
 MyApp.name = "Agora possui outro nome"
 console.log(global.MyApp.name)
