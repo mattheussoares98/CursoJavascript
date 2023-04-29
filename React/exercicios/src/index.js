137de504c1837747b0c243e18b7b2dbe15e06eff
Mattheus Soares <mattheussbarbosa@hotmail.com>
2023-04-29 16:30:06 -0300

    att

---
+++ React/exercicios/src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//renderiza o primeiro elemento dentro da página
+ReactDOM.render(<h1>React</h1>, document.getElementById("root"))
+//vai inserir "react" na página
