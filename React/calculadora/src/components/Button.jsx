b06351718d5fe955c8bc618ffa3e7cb5f3947388
Mattheus Soares <mattheussbarbosa@hotmail.com>
2023-05-04 22:38:21 -0300

    att

--- React/calculadora/src/components/Button.jsx
+++ React/calculadora/src/components/Button.jsx
@@ -1,4 +1,14 @@
 import React from "react";
 import "./Button.css"
 
-export default props => <button className="button">{props.label}</button>
+export default props => {
+    let classes = "button"
+    if (props.operation) classes += " operation"
+    if (props.double) classes += " double"
+    if (props.triple) classes += " triple"
+
+    return <button
+        onClick={e => props.click && props.click(e.target.innetHTML)}
+        className={classes}>{props.label}
+    </button>
+}
--- React/calculadora/src/main/Calculator.jsx
+++ React/calculadora/src/main/Calculator.jsx
@@ -1,28 +1,50 @@
 import React, { Component } from "react";
 import "./Calculator.css"
 import Button from "../components/Button";
+import Display from "../components/Display";
 
 export default class Calculator extends Component {
+
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+        // this.setOperation = this.setOperation.bind(this)
+    }
+    clearMemory() {
+        console.log("limpar")
+    }
+
+    setOperation(operation) {
+        console.log(operation)
+    }
+
+    addDigit(n) {
+        console.log(n)
+    }
+
+
     render() {
-        const element = <div className="calculator">
-            <Button label="AC"></Button>
-            <Button label="/"></Button>
-            <Button label="7"></Button>
-            <Button label="8"></Button>
-            <Button label="9"></Button>
-            <Button label="*"></Button>
-            <Button label="4"></Button>
-            <Button label="5"></Button>
-            <Button label="6"></Button>
-            <Button label="-"></Button>
-            <Button label="1"></Button>
-            <Button label="2"></Button>
-            <Button label="3"></Button>
-            <Button label="+"></Button>
-            <Button label="0"></Button>
-            <Button label="."></Button>
-            <Button label="="></Button>
-        </div>
+        const element = (<div className="calculator">
+            <Display value={100}></Display>
+            <Button label="AC" operation triple click={this.clearMemory}></Button>
+            <Button label="/" operation click={e => this.setOperation("/")}></Button>
+            <Button label="7" click={e => this.addDigit("7")}></Button>
+            <Button label="8" click={e => this.addDigit("8")}></Button>
+            <Button label="9" click={e => this.addDigit("9")}></Button>
+            <Button label="*" operation click={e => this.setOperation("*")}></Button>
+            <Button label="4" click={e => this.addDigit("4")}></Button>
+            <Button label="5" click={e => this.addDigit("5")}></Button>
+            <Button label="6" click={e => this.addDigit("6")}></Button>
+            <Button label="-" operation click={e => this.setOperation("-")}></Button>
+            <Button label="1" click={e => this.addDigit("1")}></Button>
+            <Button label="2" click={e => this.addDigit("2")}></Button>
+            <Button label="3" click={e => this.addDigit("3")}></Button>
+            <Button label="+" operation click={e => this.setOperation("+")}></Button>
+            <Button label="0" double click={e => this.addDigit("0")}></Button>
+            <Button label="." click={e => this.addDigit(".")}></Button>
+            <Button label="=" operation click={e => this.setOperation("=")}></Button>
+        </div>)
         return element;
     }
 }
