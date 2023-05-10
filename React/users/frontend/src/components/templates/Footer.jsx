e511e325a30d1ec914b5b129af6ecdd8966b46ca
Mattheus <mattheussbarbosa@hotmail.com>
2023-05-10 16:57:39 -0300

    att

--- React/users/frontend/src/components/templates/Footer.jsx
+++ React/users/frontend/src/components/templates/Footer.jsx
@@ -1,7 +1,17 @@
 import "./Footer.css"
 import React from "react"
 
-export default props => 
-<footer className="footer">
-    Rodapé
-</footer>
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido
+            <i className="fa fa-handPointLeft text-danger">
+                por
+            </i>
+            <strong>
+                Mattheus
+                <span className="text-danger">Soares  </span>
+                Barbosa
+            </strong>
+        </span>
+    </footer>
--- React/users/frontend/src/components/templates/Nav.jsx
+++ React/users/frontend/src/components/templates/Nav.jsx
@@ -3,5 +3,12 @@ import React from "react"
 
 export default props => 
 <aside className="menu-area">
-    Navegação
+    <nav className="menu">
+        <a href="#/">
+            <i className="fa fa-home">Início</i>
+        </a>
+        <a href="#/users">
+            <i className="fa fa-users">Usuários</i>
+        </a>
+    </nav>
 </aside>
