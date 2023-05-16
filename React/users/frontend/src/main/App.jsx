ef0f712578f70362f8d9b237b7d34c27e4ec9710
Mattheus Soares <mattheussbarbosa@hotmail.com>
2023-05-15 22:15:07 -0300

    att

--- React/users/frontend/src/main/App.jsx
+++ React/users/frontend/src/main/App.jsx
@@ -2,16 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "font-awesome/css/font-awesome.min.css"
 import "./App.css"
 import React from "react"
+import { HashRouter } from "react-router-dom" /* parte de navegação de react native e react web */
 
 import Logo from "../components/templates/Logo"
 import Nav from "../components/templates/Nav"
-import Home from "../components/home/Home"
 import Footer from "../components/templates/Footer"
+import Routes from "./Routes"
 
 export default props =>
-    <div className="app">
-        <Logo></Logo>
-        <Nav></Nav>
-        <Home></Home>
-        <Footer></Footer>
-    </div>
+    <HashRouter>
+        <div className="app">
+            <Logo></Logo>
+            <Nav></Nav>
+            <Routes></Routes>
+            <Footer></Footer>
+        </div>
+    </HashRouter>
--- React/users/frontend/src/main/Routes.jsx
+++ React/users/frontend/src/main/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Redirect } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "../components/home/Home";
 import User from "../components/user/user";
@@ -7,5 +7,6 @@ import User from "../components/user/user";
 export default props =>
     <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        
+        <Route exact path="/users" element={<User />}></Route>
+        <Route exact path="*" element={<Home />}></Route>
     </Routes>
