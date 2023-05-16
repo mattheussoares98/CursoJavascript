ac8703694f98431d520633454cf8f54c5d78eceb
Mattheus <mattheussbarbosa@hotmail.com>
2023-05-16 16:45:35 -0300

    att

--- React/users/frontend/src/components/user/user.jsx
+++ React/users/frontend/src/components/user/user.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Main from "../templates/Main";
-
+import axios from "axios";
 
 const headerProps = {
     icon: "users",
@@ -8,11 +8,30 @@ const headerProps = {
     subtitle: "Cadastro de usuários: Inserir, listar, alterar e excluir"
 }
 
+const baseUrl = "http://localhost:3001/users"
+const initialState = {
+    user: { name: "", email: "" },
+    lint: [],
+}
+
 export default class User extends Component {
-    render(){
+
+    state = { ...initialState }
+
+    clear() {
+        this.setState({ user: initialState.user })
+        /* vai limpar somente o usuário */
+    }
+
+    save() {
+        
+    }
+
+
+    render() {
         return (
             <Main {...headerProps}></Main>
-            
+
         )
     }
 }
