22c03a6434b2b5982ea57b152deaeaf4daf66e5e
Mattheus <mattheussbarbosa@hotmail.com>
2023-05-31 16:31:57 -0300

    att

--- NextJS/fundamentos/src/pages/index.jsx
+++ NextJS/fundamentos/src/pages/index.jsx
@@ -1,3 +1,12 @@
-export default function Teste(){
-  return "teste"
+import Link from "next/link"
+import Estiloso from "./Estiloso"
+export default function Teste() {
+  return (
+    <>
+      Para saber melhor como funciona o jsx, leia o arquivo 'readme' que está em fundamentos/src/pages
+      <br />
+      <br />
+      <Link href="Estiloso">Navegar para estiloso</Link>
+    </>
+  )
 }
