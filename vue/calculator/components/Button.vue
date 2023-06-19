e62ab88b25b79c944508b948105bb570e178dafd
Mattheus <mattheussbarbosa@hotmail.com>
2023-06-19 16:55:22 -0300

    att

---
+++ vue/calculator/components/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="button">0</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+};
+</script>
+
+<style>
+</style>
---
+++ vue/calculator/components/Display.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="display">0</div>
+</template>
+
+<script>
+export default {
+  name: "DisplayComponent",
+};
+</script>
+
+<style>
+</style>
