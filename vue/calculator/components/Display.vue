971320ce343796971898f159d46fdb82af44a92e
Mattheus Soares <mattheussbarbosa@hotmail.com>
2023-06-20 22:34:10 -0300

    att

--- vue/calculator/components/Display.vue
+++ vue/calculator/components/Display.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="display">0</div>
+  <div class="display">
+    {{value}}
+  </div>
 </template>
 
 <script>
 export default {
   name: "DisplayComponent",
+  props: ["value"],
 };
 </script>
 
 <style>
+.display {
+  grid-column: span 4;
+  background-color: #0004;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  padding: 6px;
+  font-size: 2.1em;
+  overflow: hidden;
+  
+}
 </style>
--- vue/calculator/main/Calculator.vue
+++ vue/calculator/main/Calculator.vue
@@ -1,31 +1,61 @@
 <template>
-    <div class="calculator">
-    <Display></Display>
-    <Button></Button>
-    <Button></Button>
-    <Button></Button>
-    </div>
+  <div class="calculator">
+    <DisplayVue :value="1000"></DisplayVue>
+    <ButtonVue
+      label="AC"
+      :triple="true"
+      :operation="true"
+      @onClick="clearMemory"
+    ></ButtonVue>
+    <ButtonVue label="/" operation @onClick="setOperation" />
+    <ButtonVue label="7" @onClick="addDigit" />
+    <ButtonVue label="8" @onClick="addDigit" />
+    <ButtonVue label="9" @onClick="addDigit" />
+    <ButtonVue label="*" operation @onClick="setOperation" />
+    <ButtonVue label="4" @onClick="addDigit" />
+    <ButtonVue label="5" @onClick="addDigit" />
+    <ButtonVue label="6" @onClick="addDigit" />
+    <ButtonVue label="-" operation @onClick="setOperation" />
+    <ButtonVue label="1" @onClick="addDigit" />
+    <ButtonVue label="2" @onClick="addDigit" />
+    <ButtonVue label="3" @onClick="addDigit" />
+    <ButtonVue label="+" operation @onClick="setOperation" />
+    <ButtonVue label="0" double @onClick="addDigit" />
+    <ButtonVue label="." @onClick="addDigit" />
+    <ButtonVue label="=" operation @onClick="setOperation" />
+  </div>
 </template>
 
 <script>
-import Button from "../components/Button";
-import Display from '../components/Display';
+import ButtonVue from "../components/Button.vue";
+import DisplayVue from "../components/Display.vue";
 
 export default {
-  components: { Display, Button },
+  components: { DisplayVue, ButtonVue },
   name: "CalculatorComponent",
+  methods: {
+    clearMemory() {
+      console.log("limpar memória");
+    },
+    setOperation(operation) {
+      console.log("operação" + operation);
+    },
+    addDigit(n) {
+      console.log("Dígito" + n);
+    },
+  },
 };
 </script>
 
 <style>
 .calculator {
-    height: 320px;
-    width: 235px;
-    border-radius: 5px;
-    overflow: hidden;
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
 
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>
