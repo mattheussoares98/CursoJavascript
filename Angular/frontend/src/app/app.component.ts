af48eb247a4fcf3f97897dca54c86ad425df69ff
Mattheus <mattheussbarbosa@hotmail.com>
2023-06-26 17:00:26 -0300

    att

--- Angular/frontend/src/app/app.component.ts
+++ Angular/frontend/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nome = 'Mattheus';
+  sobrenome = 'Soares Barbosa';
 }
--- Angular/frontend/src/app/app.module.ts
+++ Angular/frontend/src/app/app.module.ts
@@ -5,21 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatListModule } from "@angular/material/list"
 
 @NgModule({
   declarations: [
+    //todos componentes que fazem parte desse módulo app.module.ts
     AppComponent,
     HeaderComponent,
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    //componente que vai ser carregado nesse módulo. Ele só é usado no module principal da aplicação.
+    AppComponent,
+  ],
+  exports: [
+    //tudo que é exportado daqui pra fora desse componente
+    //como aqui é o módulo principal, não precisa exportar nada
+    //é como se fosse uma árvore/pirâmide e o app.module.ts está no topo da hierarquia
+  ]
 })
 export class AppModule { }
