8f2138d003ed5f363971bccf8f07e481a1c09d10
Mattheus Soares <mattheussbarbosa@hotmail.com>
2023-07-26 23:09:39 -0300

    att

--- Angular/frontend/src/app/app.module.ts
+++ Angular/frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +26,10 @@ import { ProductRead2Component } from './components/product/product-read2/produc
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import localePt from "@angular/common/locales/pt"
+import { registerLocaleData } from "@angular/common"
+
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -60,7 +64,10 @@ import { MatSortModule } from '@angular/material/sort';
     MatPaginatorModule,
     MatSortModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: "pt-BR"
+  }],
   bootstrap: [
     //componente que vai ser carregado nesse módulo. Ele só é usado no module principal da aplicação.
     AppComponent,
