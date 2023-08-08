59b8ebe90c93506b00ad04a9b12fed7eedd3b541
Mattheus <mattheussbarbosa@hotmail.com>
2023-08-08 17:00:41 -0300

    att

---
+++ BancoNaoRelacional/README.sql
@@ -0,0 +1,7 @@
+--para conseguir executar os comandos desses txts via CMD, precisa instalar o mongo e adicionar a pasta do "bin" no path das variáveis do sistema
+
+-- consultar comandos
+db.help()
+
+-- consultar mais comandos ainda disponíveis
+-- digite "db." no CMD e aperte TAB
